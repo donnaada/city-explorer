1123243d574cf627ae921b358c9fa3f98ce617b7
Donna Ada <donnaada@icloud.com>
2023-04-18 06:57:53 +1000

    updaing with wrrc and logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Main from './Components/Main';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Main />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
