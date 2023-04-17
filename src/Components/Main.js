f558782117aa24fdb71213a03ad7aefe4a897542
Donna Ada <donnaada@icloud.com>
2023-04-18 05:36:18 +1000

    adding components folder and main.js

---
+++ src/Components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Main extends Component {
+    render() { 
+        return (
+            <>
+            </>
+        );
+    }
+}
+ 
+export default Main;
