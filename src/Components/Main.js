94303f4098e0b567ee869dd298bf282903c42787
Donna Ada <donnaada@icloud.com>
2023-04-18 16:18:47 +1000

    pushing feature 2

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,72 +1,91 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Form from 'react-bootstrap/Form';
-import Button from'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state={
-          city:'',
-          cityData:[]
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityData: [],
+      city: '',
+      lon: '',
+      lat: '',
+      letsExplore: false
     }
+  }
 
-    handleInput = (e) =>{
-      e.preventDefault();
+  handleInput = (e) => {
+    e.preventDefault();
+    this.setState({
+      city: e.target.value
+    })
+  }
+
+  /**
+   * 
+   * Region 1: US 
+   * GET https://us1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json
+   * 
+   * Region 2: Europe 
+   * GET https://eu1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json
+   * 
+  */
+
+  getCityData = async (e) => {
+    e.preventDefault();
+    try {
+      let api = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
+      let cityData = await axios.get(api);
+
       this.setState({
-        city: e.target.value
-      })
-    }
+        cityData: cityData.data[0],
+        lon: cityData.data[0].lon,
+        lat: cityData.data[0].lat,
+        cityName: cityData.data[0].display_name,
+        letsExplore: true
+      });
 
-    /**
-     * 
-     * Region 1: US 
-     * GET https://us1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json
-     * 
-     * Region 2: Europe 
-     * GET https://eu1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json
-     * 
-    */
+      console.log(cityData.data[0]);
+      console.log(cityData.data[0].lon);
 
-    getCityData = async (e) => {
-      e.preventDefault();
-      try {
-        let locationURL = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
+    } catch (error) {
 
-        let cityData = await axios.get(locationURL);
-  
-        this.setState({
-          cityData: cityData.data[0],
-        })
-
-        console.log(cityData.data[0]);
-
-      } catch (error) { 
-        
-      };
+    };
+  }
 
 
-    }
+  render() {
+    return (
+      <>
+        <Container>
+          <Form onSubmit={this.getCityData}>
 
-
-
-
-    render() { 
-        return (
-            <>
-            <Container>
-              <Form onSubmit={this.getCityData}>
-                <Form.Label>Enter a City:</Form.Label>
-                <Form.Control type="text" placeholder="City Name" onInput={this.handleInput}/>
-                <Button onClick={this.getCityData}>Explore!</Button>
-              </Form>
-            </Container>
-            </>
-        );
-    }
+            <InputGroup>
+              <Form.Control type="text" placeholder="Where do you want to explore?" onInput={this.handleInput} />
+              <Button onClick={this.getCityData}>Explore!</Button>
+            </InputGroup>
+          </Form>
+        </Container>
+        <Container>
+          <Card>
+            <Card.Header>
+              <h2 className='fs-3'>{!this.state.letsExplore ? 'Adventure Awaits' : this.state.cityName}</h2 >
+            </Card.Header>
+            <Card.Body>
+              <p className='fs-5'>Longitude: {this.state.lon}</p>
+              <p className='fs-5'>Latitude: {this.state.lat}</p>
+            </Card.Body>
+          </Card>
+        </Container >
+      </>
+    );
+  }
 }
- 
+
+
 export default Main;
